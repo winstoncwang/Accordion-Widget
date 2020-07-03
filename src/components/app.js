5bdb51a1357971af0ebb44a562ce4b1024e97e7f
winstoncwang <congwang@live.co.uk>
2020-07-03 14:09:38 +0100

    v1.2
    Hard code items object for display as an accordion list

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+const items = [
+	{
+		title   : 'What is React?',
+		content : 'React is a front end javascript framework'
+	},
+	{
+		title   : 'Why use React?',
+		content :
+			'React allows rapid development of the front end part of the project'
+	},
+	{
+		title   : 'How do you use React?',
+		content :
+			'Create React project by using components, either class/function components using state'
+	}
+];
+
 const App = () => {
-	return <div>Accordion</div>;
+	return <Accordion />;
 };
 
 export default App;
