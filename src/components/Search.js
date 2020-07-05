c0819b07990b35a42d232bfd85d5084b8ddc8e43
winstoncwang <congwang@live.co.uk>
2020-07-05 23:03:55 +0100

    v1.6
    axios async request with wiki api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 	const [ term, setTerm ] = useState('');
+	const [ result, setResult ] = useState([]);
 
-	useEffect(() => {
-		console.log('asda');
-	}, []);
+	useEffect(
+		() => {
+			const search = async () => {
+				const {
+					data
+				} = await axios.get('https://en.wikipedia.org/w/api.php', {
+					params : {
+						action   : 'query',
+						list     : 'search',
+						origin   : '*',
+						format   : 'json',
+						srsearch : term
+					}
+				});
+
+				setResult(data.query.search);
+			};
+
+			if (term) {
+				search();
+			}
+		},
+		[ term ]
+	);
 
 	return (
 		<div className="ui container">
