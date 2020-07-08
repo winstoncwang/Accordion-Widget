24e7f86ab14457f48ca240ea11cb608bc5091111
winstoncwang <congwang@live.co.uk>
2020-07-08 15:13:20 +0100

    v2.3
    fixing dropdown not closing after clicking area not created by dropdown component using useRef

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+	const [ open, setOpen ] = useState(false);
+	const ref = useRef();
+
+	useEffect(() => {
+		document.body.addEventListener('click', (event) => {
+			if (ref.current.contains(event.target)) {
+				return;
+			}
+			setOpen(false);
+		});
+	}, []);
+
 	const renderedOptions = options.map((option) => {
+		if (option.value === selected.value) {
+			return null;
+		}
+
 		return (
 			<div
 				key={options.value}
@@ -16,14 +32,27 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 	});
 
 	return (
-		<div className="ui form">
-			<div className="field">
-				<label className="label">Select a Color</label>
-				<div className="ui selection dropdown visible active">
-					<i className="dropdown icon" />
-					<div className="text">{selected.label}</div>
-					<div className="menu visible transition">
-						{renderedOptions}
+		<div className="ui container" ref={ref}>
+			<div className="ui form">
+				<div className="field">
+					<label className="label">Select a Color</label>
+					<div
+						onClick={() => {
+							setOpen(!open);
+						}}
+						className={`ui selection dropdown ${open
+							? 'visible active'
+							: ''}`}
+					>
+						<i className="dropdown icon" />
+						<div className="text">{selected.label}</div>
+						<div
+							className={`menu ${open
+								? 'visible transition'
+								: ''}`}
+						>
+							{renderedOptions}
+						</div>
 					</div>
 				</div>
 			</div>
