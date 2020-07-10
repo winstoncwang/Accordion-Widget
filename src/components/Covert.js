037c3734c296977a46533edf15cf4b4965a3a9bf
winstoncwang <congwang@live.co.uk>
2020-07-10 17:51:33 +0100

    v2.7
    convert component created with input text and language, will be used to translate via google translate api

---
+++ src/components/Covert.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Convert = ({ language, input }) => {
+	useEffect(
+		() => {
+			console.log(text);
+		},
+		[ language, input ]
+	);
+
+	return <div />;
+};
+
+export default Convert;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Covert';
 
 const options = [
 	{
@@ -36,6 +39,9 @@ const Translate = () => {
 					onSelectedChange={setLanguage}
 					options={options}
 				/>
+				<hr />
+				<h3 className="ui header">Output</h3>
+				<Convert input={input} language={language} />
 			</div>
 		</div>
 	);
